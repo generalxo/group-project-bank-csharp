eb3cab28bf7215333b91281ce335ee547494c9f1
Oliver Wasner <oli.wasner@hotmail.com>
2023-01-16 23:41:24 +0100

    AsciiArt

--- Program.cs
+++ Program.cs
@@ -4,7 +4,141 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            StartMenu();
         }
+
+        public static void AsciiArt()
+        {
+            Console.Clear();
+
+            string asciiLayer1 = "         .___,   ";
+            string asciiLayer2 = "      ___('v')___";
+            string asciiLayer3 = "      '\"-\\._./-\"'";
+            string asciiLayer4 = "          ^ ^    ";
+
+            Console.WriteLine();
+            Console.WriteLine(asciiLayer1);
+            Console.WriteLine(asciiLayer2);
+            Console.WriteLine(asciiLayer3);
+            Console.WriteLine(asciiLayer4);
+            Console.WriteLine();
+            Console.WriteLine(" Welcome to Owl Banking");
+            Console.WriteLine(" ----------------------");
+
+            Help();
+
+        }
+
+        static int menuIndex = 0;
+        public static void StartMenu()
+        {
+            AsciiArt();
+
+            string menuMsg = " Welcome to Owl Banking\n Please select an option";
+
+            //Declaration of list and items.
+            List<string> menuItems = new()
+            {
+                "Log in",
+                "Help",
+                "Exit"
+            };
+
+            while (true)
+            {
+                string selectedMenuItem = DrawMenu(menuItems, menuMsg);
+                //Switch for the selectedMenuItem string that DrawMenu returns
+                switch (selectedMenuItem)
+                {
+                    case "Log in":
+                        Console.WriteLine(" Login would start here");
+                        Console.WriteLine(" Enter any key to continue");
+                        Console.ReadKey();
+                        break;
+                    case "Help":
+                        Help();
+                        break;
+                    case "Exit":
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+
+        }
+
+        public static string DrawMenu(List<string> menuItem, string menuMsg)
+        {
+            //Method takes in a List of strings and displays them. The static int menuIndex indicates which item the user is selecting
+            //Method allows user to navigare the List of strings and select a string and return it
+
+            Console.Clear();
+            Console.WriteLine("");
+
+            //Msg that can be displayed in menu
+            Console.WriteLine(menuMsg);
+            Console.WriteLine("");
+
+            //looping through menuItems and displaying them. if menuIndex == i add "["  "]" to menuItem[i]
+            for (int i = 0; i < menuItem.Count; i++)
+            {
+                if (i == menuIndex)
+                {
+                    Console.WriteLine($"[{menuItem[i]}]");
+                }
+                else
+                {
+                    Console.WriteLine($" {menuItem[i]} ");
+                }
+            }
+
+            //Check for key input
+            ConsoleKeyInfo ckey = Console.ReadKey();
+
+            //Down arrow key check
+            if (ckey.Key == ConsoleKey.DownArrow)
+            {
+                if (menuIndex == menuItem.Count - 1) { }
+                else { menuIndex++; }
+            }
+            //Up arrow key check
+            else if (ckey.Key == ConsoleKey.UpArrow)
+            {
+                if (menuIndex <= 0) { }
+                else { menuIndex--; }
+            }
+            //Left arrow key check
+            else if (ckey.Key == ConsoleKey.LeftArrow)
+            {
+                //Console.Clear();
+            }
+            //Right arrow key check
+            else if (ckey.Key == ConsoleKey.RightArrow)
+            {
+                return menuItem[menuIndex];
+            }
+            //Enter key check
+            else if (ckey.Key == ConsoleKey.Enter)
+            {
+                return menuItem[menuIndex];
+            }
+            else
+            {
+                return "";
+            }
+
+            return "";
+        }
+
+        public static void Help()
+        {
+            Console.WriteLine("");
+            Console.WriteLine(" You can navigate the menu by using up and down arrows");
+            Console.WriteLine(" To select an option press the enter key or the right arrow key!");
+            Console.WriteLine("");
+            Console.WriteLine(" Press any key to continue");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
     }
 }
